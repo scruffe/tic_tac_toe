4bdf5df6e8c622af7d0c24563baf1a2067b9088c
scruffe <78rely0d@gmail.com>
2023-02-05 23:32:54 +0100

    first try

--- app/src/main/java/com/example/myapplication123/Activity2.kt
+++ app/src/main/java/com/example/myapplication123/Activity2.kt
@@ -11,82 +11,125 @@ import java.util.*
 
 class Activity2 : AppCompatActivity() {
     private var gameOver: Boolean = false
-    // 0 = O
-    // 3 = X
-    // 2 = null
-    private var activePlayer: Int = 0
-    private val gameState = IntArray(9) { 2 }
+    private var activePlayer: Int = 1
+    private val gameState = IntArray(9) {0}
     private var counter: Int = 0
+    private var highscore: Int = 0
+    private var score: Int = -1
+    private var lives: Int = 3
+    private var gamemode: Int =1 //1 = survival time attack
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+        findViewById<TextView>(R.id.status).text = java.lang.String.format(resources.getString(R.string.first_turn_text))
     }
 
     fun playerTap(view: View){
         val status = findViewById<TextView>(R.id.status)
         val imgTag = Integer.parseInt(view.tag.toString())
-        val imgView = findViewById<ImageView>(view.id)
+        val imgViewOld = findViewById<ImageView>((view.id))
 
-        if (gameOver) {
-            restartGame(view)
-            return
-        }
+        if (gamemode==1){
+            if (gameState[imgTag] == 0 && counter!=0){
+                lives--
+                if (score > highscore){
+                    highscore = score
+                }
+                score -= 3
+                if (score<0){
+                    score = 0
+                }
 
-        // if the spot is already played, ignore
-        if (gameState[imgTag] != 2){
-            return
-        }
+                if (lives < 0){
+                    restartGame(view)
+                    status.text = java.lang.String.format(resources.getString(R.string.game_over))
+//                counter = 1
+                    return
+                }
 
-        // mark this position
-        gameState[imgTag] = activePlayer
-
-        fun checkWin(winInt: Int):Boolean {
-            if(threeInARow(winInt, activePlayer)) {
-                status.text = resources.getString(R.string.game_over)
-                gameOver = true
-                return true
+                status.text = java.lang.String.format("hs $highscore, Lives $lives, score $score")
+                counter = 1
+                return
             }
-            return false
+            // clear this position
+            imgViewOld.setImageResource(0)
+
+            counter++
+            if (counter == 60){
+                // Game over
+                status.text = java.lang.String.format(resources.getString(R.string.game_over))
+                restartGame(view)
+                return
+            }
+            score++
+            status.text = java.lang.String.format("hs $highscore, Lives $lives, score $score")
+
+
+            gameState[imgTag] = 0
+
+
+            var drawable = 0
+            //val lastPlayerWinInt = activePlayer * 3
+
+            if (activePlayer == 1) {
+                val imgs = intArrayOf(R.drawable.circle1, R.drawable.circle2,R.drawable.x1, R.drawable.x2)
+                drawable = imgs[Random().nextInt(imgs.size)]
+                //activePlayer = 3
+            }
+            val view_ids = intArrayOf(R.id.imageView0,R.id.imageView1,R.id.imageView2
+                ,R.id.imageView3,R.id.imageView4,R.id.imageView5,R.id.imageView6
+                ,R.id.imageView7,R.id.imageView8)
+            val r = Random().nextInt(view_ids.size)
+            findViewById<ImageView>(view_ids[r]).setImageResource(drawable)
+
+            // mark new position
+            gameState[r] = 1
         }
 
-        var drawable = 0
-        val lastPlayerWinInt = activePlayer * 3
 
-        val rand = Random()
-        if (activePlayer == 0) {
-            val circles = intArrayOf(R.drawable.circle1, R.drawable.circle2)
-            drawable = circles[rand.nextInt(circles.size)]
-            activePlayer = 3
 
-        } else {
-            val crosses = intArrayOf(R.drawable.x1, R.drawable.x2)
-            drawable = crosses[rand.nextInt(crosses.size)]
-            activePlayer = 0
-        }
+//        fun checkWin(winInt: Int):Boolean {
+//            if(threeInARow(winInt, activePlayer)) {
+//                status.text = resources.getString(R.string.game_over)
+//                gameOver = true
+//                return true
+//            }
+//            return false
+//        }
 
-        imgView.setImageResource(drawable)
 
-        if (checkWin((lastPlayerWinInt))){
-            return
-        }
 
-        counter++
-        if (counter >= 9){
-            // Match Draw
-            status.text = java.lang.String.format(resources.getString(R.string.match_draw))
-            gameOver = true
-            return
-        }
+//        } else {
+//            val crosses = intArrayOf(R.drawable.x1, R.drawable.x2)
+//            drawable = crosses[rand.nextInt(crosses.size)]
+//            activePlayer = 0
+//        }
+
+//        imgView.setImageResource(drawable)
+//
+//        if (checkWin((lastPlayerWinInt))){
+//            return
+//        }
+
+
+
+    }
+    private fun checkHighscore(c: Int, highscore: Int): Int {
+        var x = highscore
+
+        return x
     }
 
-
     fun restartGame(view: View){
+        lives = 3
         gameOver = false
-        activePlayer = 0
+        activePlayer = 1
         counter = 0
+        score = 0
         for (i in 0 until 9) {
-            gameState[i] = 2
+            gameState[i] = 0
         }
         findViewById<ImageView>(R.id.imageView0).setImageResource(0)
         findViewById<ImageView>(R.id.imageView1).setImageResource(0)
--- app/src/main/java/com/example/myapplication123/MainActivity.kt
+++ app/src/main/java/com/example/myapplication123/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
